e7acd8da0053a21f38d965550236b3572db4a178
alexander.berkovich <toalexsmail@yahoo.com>
2019-03-12 08:15:55 +0200

    Stub for dependencies to web-service is added; Abbility to install test dependencies from setuptools is added.

--- README.rst
+++ README.rst
@@ -18,3 +18,11 @@ Requirements
 RocketPaperScissorsGame requires the following modules.
 
 * Python 3.7+
+
+pip install . # only installs "required"
+pip install .[ws]   # installs ws dependencies
+pip install .[test] # installs dependencies for tests
+
+#from the directory with setup.py
+python setup test #run all tests
+pytest -o log_cli=true --log-cli-level=DEBUG
--- setup.py
+++ setup.py
@@ -10,8 +10,11 @@ def get_content(filename):
     return content
 
 install_requires = get_content('requirements.txt')
-tests_require = get_content('requirements-test.txt')
-
+tests_require = get_content('requirements-tests.txt')
+extras = {
+    'ws': get_content('requirements-ws.txt'),
+    'test': tests_require
+}
 
 
 setup(
@@ -26,27 +29,41 @@ setup(
     ]),
     packages=setuptools.find_packages(exclude=('tests*',)),
     install_requires=install_requires,
+    extras_require=extras,
+    test_suite="tests",
     tests_require=tests_require,
-    test_suite = "tests",
+    setup_requires=['pytest-runner'],
     namespace_packages=('alexber',),
     license='Apache 2.0',
     keywords='game engine player rock papaer scissors',
     classifiers=[
         # See: https://pypi.python.org/pypi?:action=list_classifiers
         'Development Status :: 1 - Planning',
-        'Environment :: Console'
+        'Environment :: Console',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
+        'Intended Audience :: Education',
         'License :: OSI Approved :: BSD License',
 
         # List of python versions and their support status:
         # https://en.wikipedia.org/wiki/CPython#Version_history
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython'
         "Topic :: Utilities",
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Desktop Environment',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Games/Entertainment :: Board Games',
+        'Topic :: Education',
+        'Operating System :: OS Independent',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Natural Language :: English',
     ],
+    python_requires='>=3.7',
     zip_safe= False,
     include_package_data=True
 )
