756dd059f3d0850f582c385fec6ed67ed209cb97
alexander.berkovich <toalexsmail@yahoo.com>
2019-04-04 08:37:38 +0300

    Added unit for java properties with alternative delim

--- tests/test_utils/properties_test.py
+++ tests/test_utils/properties_test.py
@@ -6,6 +6,24 @@ import pytest
 from alexber.utils.props import Properties
 
 
+def test_parse_java_properties_alternative_delim(request, mocker):
+    logger.info(f'{request._pyfuncitem.name}()')
+    mocker.patch.object(Properties, 'PLACEHOLDER_TOKEN', new_callable=lambda :'@')
+    exp_cls_name_a = 'alexber.rpsgame.players.ConstantPlayer'
+    exp_cls_name_b = exp_cls_name_a
+    expdd = {'playera.cls': 'alexber.rpsgame.players.ConstantPlayer',
+             'playerb.cls': 'alexber.rpsgame.players.ConstantPlayer', }
+
+    dir = Path(__file__).parent
+
+    p = Properties()
+    with open(dir / 'config2.properties') as f:
+        p.load(f)
+    dd = p.as_dict()
+
+    assert expdd == dd
+
+
 def test_parse_java_properties(request):
     logger.info(f'{request._pyfuncitem.name}()')
     exp_cls_name_a = 'alexber.rpsgame.players.ConstantPlayer'
