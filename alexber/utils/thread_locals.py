def4336cbb447e86c87afe38774d8b6ec6b175d4
alexander.berkovich <toalexsmail@yahoo.com>
2019-04-21 13:06:26 +0300

    threadlocal_var is added

---
+++ alexber/utils/thread_locals.py
@@ -0,0 +1,20 @@
+import threading
+
+
+#inspired by https://stackoverflow.com/questions/1408171/thread-local-storage-in-python
+thread_locals = threading.local()
+
+#TODO: Alex add tests
+
+def threadlocal_var(varname, factory, *args, **kwargs):
+  v = getattr(thread_locals, varname, None)
+  if v is None:
+    v = factory(*args, **kwargs)
+    setattr(thread_locals, varname, v)
+  return v
+
+def get_threadlocal_var(varname):
+    v = threadlocal_var(varname, lambda : None)
+    if v is None:
+        raise ValueError(f"threadlocal's {varname} is not initilized")
+    return v
